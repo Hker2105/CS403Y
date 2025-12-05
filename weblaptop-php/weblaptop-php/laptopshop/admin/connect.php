ad61f1389942b6e542b7962274131475d023c3d7
Toantruong0611 <toantruongquang05@gmail.com>
2025-12-05 07:17:43 +0700

    Update connect.php

--- weblaptop-php/weblaptop-php/laptopshop/admin/connect.php
+++ weblaptop-php/weblaptop-php/laptopshop/admin/connect.php
@@ -1,9 +1,8 @@
 <?php
+$conn = mysqli_connect("127.0.0.1", "root", "", "laptop", 3306);
 
-$conn = mysqli_connect('127.0.0.1', 'root', '', 'laptop', 3306);
-
-mysqli_set_charset($conn, 'UTF8');
-if (mysqli_connect_errno()) {
-	echo "kết nối không thành công";
+if (!$conn) {
+    die("❌ KẾT NỐI DATABASE THẤT BẠI: " . mysqli_connect_error());
 }
-?>
+
+mysqli_set_charset($conn, "utf8mb4");
